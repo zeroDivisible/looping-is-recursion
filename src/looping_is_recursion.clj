a15528a0c6448612c106ad5acdb41aa7dc5a6566
zerodivisible <zerodivisible@gmail.com>
2014-08-13 16:46:01 +0200

    added first two functions

--- src/looping_is_recursion.clj
+++ src/looping_is_recursion.clj
@@ -1,10 +1,20 @@
 (ns looping-is-recursion)
 
 (defn power [base exp]
-  ":(")
+  (let [helper (fn [acc base exp]
+                 (if
+                   (zero? exp)
+                   acc
+                   (recur (* acc base) base (dec exp))))]
+    (helper 1 base exp)))
 
 (defn last-element [a-seq]
-  ":(")
+  (let [helper (fn [elem a-seq]
+                 (if
+                   (empty? a-seq)
+                   elem
+                   (recur (first a-seq) (rest a-seq))))]
+    (helper (first a-seq) (rest a-seq))))
 
 (defn seq= [seq1 seq2]
   ":(")
